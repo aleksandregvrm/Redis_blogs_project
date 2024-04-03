8e9c77615c9d7954ac33294d5f6aa79ded4212ad
aleksandregvrm <111559898+aleksandregvrm@users.noreply.github.com>
2024-04-03 23:06:57 +0400

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    "mongodb+srv://aleksandregvaramia:aleksandregvaramia@cluster0.cegkbto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "somemongouri",
   cookieKey: "123123123",
 };
